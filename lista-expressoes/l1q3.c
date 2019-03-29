bd9ad032938a78ab724067fe8c10452ed448926e
yuregsf <yuregsfpb@gmail.com>
2019-03-29 16:00:44 -0300

    Correção nas questões 3 e 9 da Lista de Expressões

--- lista-expressoes/l1q3.c
+++ lista-expressoes/l1q3.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#define custoAluminioM2 100.00
 #define PI 3.14159
-    
+
 int main()
 {
-    double raio, altura, areaCirculo, areaLateral, areaCilindro;
-    scanf("%lf", &raio);
-    scanf("%lf", &altura);
-    areaCirculo = PI*raio*raio;
-    areaLateral = 2*PI*raio*altura;
-    areaCilindro = 2*areaCirculo+areaLateral;
-    
-    printf("O VALOR DO CUSTO E = %.2lf\n", areaCilindro*100);
-    
+    float raio, altura, areaCilindro, custoTotal;
+    scanf("%f %f", &raio, &altura);
+    areaCilindro = 2 * (PI * raio * raio) + (2 * PI * raio * altura);
+    custoTotal = areaCilindro * custoAluminioM2;
+
+    printf("O VALOR DO CUSTO E = %.2f\n", custoTotal);
+
     return 0;
 }
--- lista-expressoes/l1q9.c
+++ lista-expressoes/l1q9.c
@@ -2,10 +2,14 @@
 #include <math.h>
 
 int main(){
-    float numero;
+    float numero, temp1, temp2, temp3;
     scanf("%f", &numero);    
-    printf("%f\n", (int)numero + round((numero - (int)numero)*10)/10);
-    printf("%f\n", (int)numero + round((numero - (int)numero)*100)/100);
-    printf("%f\n", (int)numero + round((numero - (int)numero)*1000)/1000);
+    temp1 = (int)numero + round((numero - (int)numero)*10)/10;
+    temp2 = (int)numero + round((numero - (int)numero)*100)/100;
+    temp3 = (int)numero + round((numero - (int)numero)*1000)/1000;
+
+    printf("%f\n", temp1);
+    printf("%f\n", temp2);
+    printf("%f\n", temp3);
     return 0;
 }
