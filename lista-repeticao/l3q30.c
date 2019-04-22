8fc63dcfd798abe03027a05d38a037cafe5640ea
yuregsf <yuregsfpb@gmail.com>
2019-04-22 17:48:38 -0300

    terminei finalmente

--- lista-repeticao/l3q30.c
+++ lista-repeticao/l3q30.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    int n, i, j, menorAmigo = 1, maiorAmigo = 1, somador1=0, somador2=0;
-    scanf("%d", &n);
+    int i, j, vezes, n = 5, m, soma = 0;
+    scanf("%d", &vezes);
     
-    for(i=0; i<n; i++){
+    for(i=0; i<vezes; i++){
         while(1){
-            while(somador1 <= menorAmigo){
-                somador1++;
-            }
-            maiorAmigo = somador1;
-            for(j = 0; j < maiorAmigo; j++){
-                if(maiorAmigo%j){
-                    somador2++;
+            soma = 0;
+            for(j = 1; j < n; j++){
+                if(n%j == 0){
+                    soma += j;
                 }
             }
-            if(somador2 == menorAmigo && menorAmigo < maiorAmigo){
-                printf("(%d,%d)", menorAmigo, maiorAmigo);
+            m = soma;
+            soma = 0;
+            for(j = 1; j<m; j++){
+                if(m%j == 0){
+                    soma += j;
+                }
+            }
+            if(soma == n && n < m){
+                printf("(%d,%d)\n", n, m);
+                n++;
                 break;
             }else{
-                menorAmigo++;
+                n++;
             }
         }
     }
